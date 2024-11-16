3630a72f8e97c17e27396445c8fe611635abb5a4
Tõnis Ojandu <tonis.ojandu@sympower.net>
2024-11-16 14:28:02 +0200

    Add retry-until-all-merged feature

--- cmd/renovator/renovator.go
+++ cmd/renovator/renovator.go
@@ -9,12 +9,13 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	ctx := context.Background()
 	var token, tokenVariable, org, user, author, dependency, defaultComment string
-	var yes, debug bool
+	var yes, debug, retryUntilAllMerged bool
 
 	flag.StringVar(&token, "token", "", "GitHub token to use")
 	flag.StringVar(&tokenVariable, "token-variable", "", "Name of an environment variable to read GitHub token from")
@@ -25,6 +26,7 @@ func main() {
 	flag.StringVar(&defaultComment, "m", "LGTM", "The default comment for PR approvals")
 	flag.BoolVar(&yes, "y", false, "Approve all matching PR-s")
 	flag.BoolVar(&debug, "debug", false, "Enables additional output")
+	flag.BoolVar(&retryUntilAllMerged, "retry-until-all-merged", false, "Retry until all PR-s are merged")
 	flag.Parse()
 
 	if token == "" && tokenVariable == "" {
@@ -77,90 +79,112 @@ func main() {
 		fmt.Printf("Found %d renovate PRs\n", len(matchingPRs))
 	}
 
-	// Process each PR
-	for _, pr := range matchingPRs {
-		fmt.Printf("Processing PR: %s\n", *pr.Title)
-		repoUrl := pr.GetHTMLURL()
-		fmt.Printf("Repo URL: %s\n", repoUrl)
+	// Retry logic
+	for {
+		// Process each PR
+		for _, pr := range matchingPRs {
+			fmt.Printf("Processing PR: %s\n", *pr.Title)
+			repoUrl := pr.GetHTMLURL()
+			fmt.Printf("Repo URL: %s\n", repoUrl)
 
-		// get third last element from the URL into repoName
-		repoName := strings.Split(repoUrl, "/")[4]
-		if repoName == "" {
-			log.Printf("Cannot get repository name for PR: %s", *pr.Title)
-			continue
+			// get third last element from the URL into repoName
+			repoName := strings.Split(repoUrl, "/")[4]
+			if repoName == "" {
+				log.Printf("Cannot get repository name for PR: %s", *pr.Title)
+				continue
+			}
+			prDetails, _, err := client.PullRequests.Get(ctx, org, repoName, pr.GetNumber())
+			if err != nil {
+				log.Printf("Error fetching PR details: %v", err)
+				continue
+			}
+			if prDetails == nil {
+				log.Printf("PR details are nil for PR: %s", *pr.Title)
+				continue
+			}
+			if err != nil {
+				log.Printf("Error fetching PR details: %v", err)
+				continue
+			}
+
+			if prDetails.GetMerged() {
+				fmt.Printf("PR %s is already merged\n", *pr.Title)
+				continue
+			}
+
+			if !prDetails.GetMergeable() {
+				fmt.Printf("PR %s cannot be merged\n", *pr.Title)
+				continue
+			}
+
+			// Check if all checks are successful
+			checks, _, err := client.Checks.ListCheckRunsForRef(ctx, org, repoName, prDetails.Head.GetSHA(), nil)
+			if err != nil {
+				log.Printf("Error fetching check runs: %v", err)
+				continue
+			}
+
+			allChecksPassed := true
+			for _, check := range checks.CheckRuns {
+				if check.GetConclusion() != "success" && check.GetConclusion() != "skipped" {
+					allChecksPassed = false
+					break
+				}
+			}
+
+			if !allChecksPassed {
+				fmt.Printf("PR %s has non-succeeded checks\n", *pr.Title)
+				continue
+			}
+
+			// Ask for user approval before proceeding
+			if confirmMerge(*pr.Title) {
+				// Approve the PR
+				review := &github.PullRequestReviewRequest{
+					Body:  github.String("LGTM"),
+					Event: github.String("APPROVE"),
+				}
+				_, _, err = client.PullRequests.CreateReview(ctx, org, repoName, *pr.Number, review)
+				if err != nil {
+					log.Printf("Error approving PR: %v", err)
+					continue
+				}
+
+				// Merge the PR
+				options := &github.PullRequestOptions{
+					MergeMethod: "rebase",
+				}
+				_, _, err = client.PullRequests.Merge(ctx, org, repoName, *pr.Number, "", options)
+				if err != nil {
+					log.Printf("Error merging PR: %v", err)
+					continue
+				}
+
+				fmt.Printf("Successfully merged PR: %s\n", *pr.Title)
+			} else {
+				fmt.Printf("Skipping PR: %s\n", *pr.Title)
+			}
 		}
+
+		// Check if retry is needed
+		if !retryUntilAllMerged || allPRsMerged(matchingPRs, client, ctx, org) {
+			break
+		}
+
+		fmt.Println("Some PR-s are not merged, retrying in 5 seconds")
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func allPRsMerged(prs []*github.Issue, client *github.Client, ctx context.Context, org string) bool {
+	for _, pr := range prs {
+		repoName := strings.Split(pr.GetHTMLURL(), "/")[4]
 		prDetails, _, err := client.PullRequests.Get(ctx, org, repoName, pr.GetNumber())
-		if err != nil {
-			log.Printf("Error fetching PR details: %v", err)
-			continue
-		}
-		if prDetails == nil {
-			log.Printf("PR details are nil for PR: %s", *pr.Title)
-			continue
-		}
-		if err != nil {
-			log.Printf("Error fetching PR details: %v", err)
-			continue
-		}
-
-		if prDetails.GetMerged() {
-			fmt.Printf("PR %s is already merged\n", *pr.Title)
-			continue
-		}
-
-		if !prDetails.GetMergeable() {
-			fmt.Printf("PR %s cannot be merged\n", *pr.Title)
-			continue
-		}
-
-		// Check if all checks are successful
-		checks, _, err := client.Checks.ListCheckRunsForRef(ctx, org, repoName, prDetails.Head.GetSHA(), nil)
-		if err != nil {
-			log.Printf("Error fetching check runs: %v", err)
-			continue
-		}
-
-		allChecksPassed := true
-		for _, check := range checks.CheckRuns {
-			if check.GetConclusion() != "success" && check.GetConclusion() != "skipped" {
-				allChecksPassed = false
-				break
-			}
-		}
-
-		if !allChecksPassed {
-			fmt.Printf("PR %s has non-succeeded checks\n", *pr.Title)
-			continue
-		}
-
-		// Ask for user approval before proceeding
-		if confirmMerge(*pr.Title) {
-			// Approve the PR
-			review := &github.PullRequestReviewRequest{
-				Body:  github.String("LGTM"),
-				Event: github.String("APPROVE"),
-			}
-			_, _, err = client.PullRequests.CreateReview(ctx, org, repoName, *pr.Number, review)
-			if err != nil {
-				log.Printf("Error approving PR: %v", err)
-				continue
-			}
-
-			// Merge the PR
-			options := &github.PullRequestOptions{
-				MergeMethod: "rebase",
-			}
-			_, _, err = client.PullRequests.Merge(ctx, org, repoName, *pr.Number, "", options)
-			if err != nil {
-				log.Printf("Error merging PR: %v", err)
-				continue
-			}
-
-			fmt.Printf("Successfully merged PR: %s\n", *pr.Title)
-		} else {
-			fmt.Printf("Skipping PR: %s\n", *pr.Title)
+		if err != nil || prDetails == nil || !prDetails.GetMerged() {
+			return false
 		}
 	}
+	return true
 }
 
 func confirmMerge(prTitle string) bool {
